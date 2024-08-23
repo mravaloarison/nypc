f05144a42390de49e6fd20155cd068b6fd3cba08
mravaloarison <100179286+mravaloarison@users.noreply.github.com>
2024-08-23 17:00:04 -0400

    loader fixed

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	webpack: (config) => {
+		config.module.rules.push({
+			test: /\.map$/,
+			use: "ignore-loader", // or 'null-loader' if necessary
+		});
+
+		return config;
+	},
+};
 
 export default nextConfig;
